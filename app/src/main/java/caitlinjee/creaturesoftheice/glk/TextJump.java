2e8a439fb24f3eba6e987852e06e5d83789d6d98
caitlinjee <caitlinjeespn@gmail.com>
2020-12-02 19:00:46 +0800

    Add android support

---
+++ app/src/main/java/caitlinjee/creaturesoftheice/glk/TextJump.java
@@ -0,0 +1,13 @@
+package caitlinjee.creaturesoftheice.glk;
+
+public class TextJump
+{
+	public String name;
+	public int loc;
+		
+	TextJump(String name,int loc)
+	{
+		this.name=name;
+		this.loc=loc;
+	}
+}
---
+++ app/src/main/java/caitlinjee/creaturesoftheice/vm/VMThread.java
@@ -0,0 +1,24 @@
+package caitlinjee.creaturesoftheice.vm;
+
+public class VMThread extends Thread
+{
+	private VM vm;
+	
+	public VMThread(VM vm)
+	{
+		this.vm=vm;
+	}
+
+	public void run()
+	{
+		vm.run();
+	}
+	
+	public void finalize()
+	{
+		if (vm!=null){
+			vm.cleanExit();
+			vm=null;
+		}
+	}
+}
